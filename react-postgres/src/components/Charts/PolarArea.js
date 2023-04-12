8f1e3d4ec2283f3dc7d595692a7713f8a680e953
sim24601 <labohemeheureuse@gmail.com>
2023-04-12 17:24:31 +0200

    favicon
    favicon

--- react-postgres/src/components/Charts/PolarArea.js
+++ react-postgres/src/components/Charts/PolarArea.js
@@ -2,7 +2,7 @@ import React from "react";
 import { PolarArea } from "react-chartjs-2";
 import { PolarAreaController, ArcElement, RadialLinearScale, Chart } from "chart.js";
 
-export default function BarChart(props) {
+export default function Polar(props) {
     Chart.register(PolarAreaController, ArcElement, RadialLinearScale);
     return(<div>
         <PolarArea data={props.donnee} width={props.largeur} height={props.hauteur} options={props.options} />
