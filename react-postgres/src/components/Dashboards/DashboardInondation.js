8efe10169b648d1c8f6912e9e4752685b3c5ae7b
sim24601 <labohemeheureuse@gmail.com>
2023-05-01 19:53:32 +0200

    vue inondation

--- react-postgres/src/components/Dashboards/DashboardInondation.js
+++ react-postgres/src/components/Dashboards/DashboardInondation.js
@@ -2,12 +2,19 @@ import React from "react";
 import "../../styles/CarteClimat.css";
 import carte from '../../img/carte/catnat_ino.png';
 import store from "../../store";
-import BarChart from "../Charts/Barchart";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function DashboardInondation() {
     const datadb = store.getState().commune.properties;
     const nb_catnat = datadb['inondations.et.ou.coulées.de.boue'];
-    const echelleCout = [0,1250,3750,7500,15000,30000];
+    const echelleCout = ["0€","0-2,5 k€","2,5-5 k€","5-10 k€","10-20 k€","+ de 20 k€"];
+    const echelleSp = ["0","0-10%","10-50%","50-100%","100-200%","+ de 200%"];
     const coutMoyen = echelleCout[datadb.ctinon];
     const dureeResidence = datadb.anciennete_prop/datadb.nb_pop_prop;
     const dureeResidenceMois = Math.floor(12*(dureeResidence - Math.floor(dureeResidence)))
@@ -43,15 +50,87 @@ export default function DashboardInondation() {
         <div>
             <img className="carte" src={carte}></img>
             {datadb != "" && (<div>
-                <ul><p className="texte"> Commune : {datadb.nom_commune} </p>
-                <p className="texte"> nombre d'occurences depuis 1982 : {nb_catnat} </p>
-                <p className="texte"> coût moyen d'un sinistre : {coutMoyen} € + 380 € de franchise </p>
-                <p className="texte"> durée moyenne de résidence : {Math.floor(dureeResidence)} années {dureeResidenceMois} mois</p>
-                <p className="texte"> coût complet du risque : {Math.floor(coutComplet*100)/100} € </p>
-                <p className="texte"> coût complet du risque (scenario RCP8.5): {Math.floor(coutCompletRCP85*100)/100} € </p>
-                <div style={{textAlign:"right", position: "absolute",left:"59%", width:"40%", height:"30%"}}><BarChart donnee={dataCout} largeur={10} hauteur={7} options={optionsCout}/> </div>
-                </ul>
-                </div>)}
+            <ul>
+            <TableContainer className="texte" sx={{width: "37vw"}} component={Paper}>
+            <Table aria-label="Top 5 des communes à risques" size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Rang</TableCell>
+                  <TableCell>Commune</TableCell>
+                  <TableCell>Nb.</TableCell>
+                  <TableCell>Coût Moyen</TableCell>
+                  <TableCell>Sinistre / primes</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    1
+                    </TableCell>
+                    <TableCell>Nice</TableCell>
+                    <TableCell align="center">39</TableCell>
+                    <TableCell>{echelleCout[3]}</TableCell>
+                    <TableCell>{echelleSp[2]}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    2
+                    </TableCell>
+                    <TableCell>Antibes</TableCell>
+                    <TableCell align="center">31</TableCell>
+                    <TableCell>{echelleCout[4]}</TableCell>
+                    <TableCell>{echelleSp[4]}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    3
+                    </TableCell>
+                    <TableCell>Cagnes-sur-Mer</TableCell>
+                    <TableCell align="center">31</TableCell>
+                    <TableCell>{echelleCout[3]}</TableCell>
+                    <TableCell>{echelleSp[2]}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    4
+                    </TableCell>
+                    <TableCell>Marseille</TableCell>
+                    <TableCell align="center">26</TableCell>
+                    <TableCell>{echelleCout[3]}</TableCell>
+                    <TableCell>{echelleSp[2]}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    5
+                    </TableCell>
+                    <TableCell>Roquebrune-sur-Argens</TableCell>
+                    <TableCell align="center">25</TableCell>
+                    <TableCell>{echelleCout[4]}</TableCell>
+                    <TableCell>{echelleSp[5]}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    ...
+                    </TableCell>
+                    <TableCell>...</TableCell>
+                    <TableCell align="center">...</TableCell>
+                    <TableCell>...</TableCell>
+                    <TableCell>...</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                    /
+                    </TableCell>
+                    <TableCell>{datadb.nom_commune}</TableCell>
+                    <TableCell align="center">{nb_catnat}</TableCell>
+                    <TableCell>{echelleCout[datadb.ctinon]}</TableCell>
+                    <TableCell>{echelleSp[datadb.spinon]}</TableCell>
+                  </TableRow>
+              </TableBody>
+          </Table>
+        </TableContainer>
+        </ul>
+        </div>)}
         </div>
     );
 }
