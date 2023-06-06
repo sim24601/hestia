ada9435b005d96af7e9b3600d3542bba9791cbe1
sim24601 <labohemeheureuse@gmail.com>
2023-06-06 11:29:39 +0200

    cors *
    cors *

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -6,8 +6,8 @@ const requetes = require('./requetes')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
